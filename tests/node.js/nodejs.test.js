f6ac14d36cc01b195e592b039ca4d87ca0b5ed23
Michele Riva <ciao@micheleriva.it>
2024-02-29 21:49:22 -0800

    adds missing import

--- tests/node.js/nodejs.test.js
+++ tests/node.js/nodejs.test.js
@@ -1,5 +1,6 @@
 const test = require('node:test')
 const { NLPChunker } = require('../../npm/script/nlp')
+const { FixedChunker } = require('../../npm/script/fixed')
 
 const longInput = `Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American businessman, inventor, and investor best known for co-founding the technology giant Apple Inc. Jobs was also the founder of NeXT and chairman and majority shareholder of Pixar. He was a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak.
 
